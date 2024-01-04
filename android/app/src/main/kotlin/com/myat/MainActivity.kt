b3168855992b507e976d961be6b086d92a0f608e
myat <myatminnaung@appvantage.sg>
2024-01-04 19:57:22 +0630

    chore: initial commit

---
+++ android/app/src/main/kotlin/com/myat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.myat.my_vgv_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:my_vgv_demo/app/app.dart';
+import 'package:my_vgv_demo/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
